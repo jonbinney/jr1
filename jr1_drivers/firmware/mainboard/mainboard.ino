04c1b45d08f15428f5ae0a8b33bfdf96997a1012
Jon Binney <jbinney@willowgarage.com>
2013-05-12 22:02:36 -0700

    arduino <-> pc packet transmission

---
+++ jr1_drivers/firmware/mainboard/mainboard.ino
@@ -0,0 +1,91 @@
+#define BUFFER_SIZE 1000
+unsigned char output_byte;
+unsigned char buffer[BUFFER_SIZE];
+#define SCOPE_PIN 8
+
+#define SERIAL_PREAMBLE "U9"
+#define SERIAL_PREAMBLE_LENGTH 2
+#define SERIAL_PAYLOAD_SIZE 16
+
+void send_packet(unsigned char *buf) {
+  Serial.write(SERIAL_PREAMBLE);
+  
+  unsigned char checksum = 0;
+  for(unsigned char byte_i = 0; byte_i < SERIAL_PAYLOAD_SIZE; byte_i++) {
+    Serial.write(buf[byte_i]);
+    checksum ^= buf[byte_i];
+  }
+  Serial.write(checksum);
+}
+
+unsigned char receive_packet(unsigned char *buf) {
+  int input_byte;
+  
+  // look for a preamble
+  unsigned char preamble_i = 0;
+  while(true) {
+    while(Serial.available() < 1)
+      /* wait */;
+    
+    input_byte = Serial.read();
+    
+    if(input_byte == SERIAL_PREAMBLE[preamble_i] || true) {
+      preamble_i++;
+    }
+    else {
+      preamble_i = 0;
+    }
+      
+    if(preamble_i == SERIAL_PREAMBLE_LENGTH) {
+      break;
+    }
+  }
+      
+  // read the payload
+  unsigned char buf_i = 0;
+  unsigned char checksum = 0;
+  while(buf_i < SERIAL_PAYLOAD_SIZE) {
+    while(Serial.available() < 1)
+      /* wait */;
+    input_byte = Serial.read();
+
+    buf[buf_i] = (unsigned char) input_byte;
+    checksum ^= input_byte;
+    buf_i++;
+  }
+  
+  // read the checksum
+  int expected_checksum;
+  while(Serial.available() < 1)
+      /* wait */;
+  expected_checksum = (unsigned char) Serial.read();
+  
+  if(checksum != expected_checksum)
+    return -1;
+  
+  return 0;
+}
+  
+void setup() {
+  Serial.begin(38400); 
+  /*
+  pinMode(SCOPE_PIN, INPUT);
+  
+  pinMode(3, OUTPUT); // this is output "B"
+  pinMode(11, OUTPUT); // this is output "A"
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22) | _BV(CS21) | _BV(CS20);
+  OCR2A = 20; // pin 11
+  OCR2B = 29; // pin 3
+  */
+}
+
+void loop() {
+  unsigned char serial_buf[SERIAL_PAYLOAD_SIZE];
+  
+  if(receive_packet(serial_buf) < 0)
+    send_packet((unsigned char *) "incorrect checksum!!!!!");
+  else
+    send_packet(serial_buf);
+ 
+}
